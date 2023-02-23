ef63fa58599a5897e68205d82074ed627163dd0c
binhtran21rew <102853177+binhtran21rew@users.noreply.github.com>
2023-02-23 11:11:32 +0700

    add category with APi laravel using hook react

--- src/layouts/admin/Slidebar.js
+++ src/layouts/admin/Slidebar.js
@@ -15,6 +15,10 @@ const Slidebar = () => {
                         <div className="sb-nav-link-icon"><i className="fas fa-tachometer-alt"></i></div>
                         Profile
                     </Link>
+                    <Link  to="/admin/categories" className="nav-link" >
+                        <div className="sb-nav-link-icon"><i className="fas fa-tachometer-alt"></i></div>
+                        Category
+                    </Link>
                     <div className="sb-sidenav-menu-heading">Interface</div>
                     <Link className="nav-link collapsed" to="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
                         <div className="sb-nav-link-icon"><i className="fas fa-columns"></i></div>
